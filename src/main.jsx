9a765ca972b1ac4eca6baf07eeaa6755eca1a3a1
Rostyslav Klyba <rklyba@gmail.com>
2023-03-01 20:49:24 +0200

    Set the <ErrorPage> as the 'errorElement' on the root route

--- src/main.jsx
+++ src/main.jsx
@@ -7,12 +7,15 @@ import {
 
 import './index.css'
 import Root from "./routes/root";
+import ErrorPage from './error-page';
+
 
 
 const router = createBrowserRouter([
   {
     path: "/",
     element: <Root />,
+    errorElement: <ErrorPage />,
   },
 ]);
 
