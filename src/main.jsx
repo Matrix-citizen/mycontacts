49c860b166475f9e2d053237f20be078957cb7dd
Rostyslav Klyba <rklyba@gmail.com>
2023-03-03 10:58:42 +0200

    Imported and set action on the route

--- src/main.jsx
+++ src/main.jsx
@@ -1,25 +1,25 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import {
-  createBrowserRouter,
-  RouterProvider,
-} from "react-router-dom";
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import { createBrowserRouter, RouterProvider } from 'react-router-dom';
 
-import './index.css'
-import Root, { loader as rootLoader} from "./routes/root";
+import './index.css';
+import Root, {
+  loader as rootLoader,
+  action as rootAction,
+} from './routes/root';
 import ErrorPage from './error-page';
-import Contact from "./routes/contact";
-
+import Contact from './routes/contact';
 
 const router = createBrowserRouter([
   {
-    path: "/",
+    path: '/',
     element: <Root />,
     errorElement: <ErrorPage />,
     loader: rootLoader,
+    action: rootAction,
     children: [
       {
-        path: "contacts/:contactId",
+        path: 'contacts/:contactId',
         element: <Contact />,
       },
     ],
@@ -30,4 +30,4 @@ ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <RouterProvider router={router} />
   </React.StrictMode>
-)
+);
